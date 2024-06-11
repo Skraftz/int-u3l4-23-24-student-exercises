f9fdef0c9c86a125ebb77f17888f4de39c6d3099
Skraftz <davekenkent@gmail.com>
2024-06-11 13:51:08 -0400

    e

--- domManipulation.js
+++ domManipulation.js
@@ -7,7 +7,7 @@ let image2Button = document.querySelector('.image-2-button');
 let myName = document.querySelector('.name');
 let myHobby = document.querySelector('.hobby');
 let myLocation = document.querySelector('.location');
-let image = document.querySelector('.image');
+let aga = document.querySelector('.image');
 
 
 // CODE ALONG
@@ -22,19 +22,23 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
-
-
+    myName.innerHTML = "David Kent";
+    myHobby.innerHTML = "Programming";
+    myLocation.innerHTML = "The Milky Way Galaxy";
 
 
 
 
 });
 
+image1Button.addEventListener("click", function(){
+    aga.src = "0001-0055.gif";
+});
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
 
 
-
+image2Button.addEventListener("click", function(){aga.src = "20240606_080427.jpg"})
 
 
 // 5. Add an event listener to the `image2Button`
